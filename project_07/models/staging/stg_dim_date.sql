85dc77da8cf9e357a102114e3f044bcb4e8f23e7
Hhd <hdh997@outlook.com>
2026-07-17 14:21:52 +1200

    Add staging for date and store dimensions

---
+++ project_07/models/staging/stg_dim_date.sql
@@ -0,0 +1,59 @@
+WITH stg_dim_date__source AS (
+    SELECT *
+    FROM {{ source('glamira_source', 'summary19') }}
+),
+stg_dim_date__extract AS (
+    SELECT
+        EXTRACT(year FROM time_stamp) AS year,
+        EXTRACT(month FROM time_stamp) AS month,
+        EXTRACT(day FROM time_stamp) AS day,
+        EXTRACT(hour FROM time_stamp) AS hour,
+        EXTRACT(minute FROM time_stamp) AS minute,
+        EXTRACT(second FROM time_stamp) AS second,
+        FORMAT_DATE('%A', DATE(time_stamp)) AS week_day,
+        CASE 
+            WHEN EXTRACT(DAYOFWEEK FROM time_stamp) IN (1, 7) THEN 'TRUE'
+            ELSE 'FALSE'
+        END AS is_weekend,
+        CASE 
+            WHEN EXTRACT(DAYOFYEAR FROM DATE(time_stamp)) IN (1, 2, 3, 4, 5, 6, 7) THEN 'TRUE'
+            ELSE 'FALSE'
+        END AS is_holiday
+    FROM stg_dim_date__source
+),
+stg_dim_date__process AS (
+    SELECT
+        year,
+        month,
+        day,
+        CONCAT(hour, ':', minute, ':', second) AS time,
+        week_day,
+        is_weekend,
+        is_holiday,
+    FROM stg_dim_date__extract
+),
+stg_dim_date__cast_type AS (
+    SELECT
+        CAST(year AS INT64) AS year,
+        CAST(month AS INT64) AS month,
+        CAST(day AS INT64) AS day,
+        CAST(time AS STRING) AS time,
+        CAST(week_day AS STRING) AS week_day,
+        CAST(is_weekend AS BOOLEAN) AS is_weekend,
+        CAST(is_holiday AS BOOLEAN) AS is_holiday
+    FROM stg_dim_date__process
+),
+stg_dim_date__gen_key AS (
+    SELECT
+        year,
+        month,
+        day,
+        time,
+        week_day,
+        is_weekend,
+        is_holiday,
+        FARM_FINGERPRINT(year || month || day || time) AS date_id
+    FROM stg_dim_date__cast_type
+)
+SELECT *
+FROM stg_dim_date__gen_key
---
+++ project_07/models/staging/stg_dim_store.sql
@@ -0,0 +1,92 @@
+WITH stg_dim_store__source AS (
+    SELECT *
+    FROM {{ source('glamira_source', 'summary19') }}
+), 
+stg_dim_store__process_url AS (
+    SELECT
+        concat('https://', net.host(current_url), '/') AS store_url
+
+    FROM stg_dim_store__source
+),
+stg_dim_store__filtered AS (
+    SELECT *
+    FROM stg_dim_store__process_url
+    WHERE store_url IS NOT NULL
+    AND store_url like 'https://glamira.%'
+),
+stg_dim_store__extract_extension AS (
+    SELECT
+        store_url,
+        regexp_extract(net.host(store_url), r'\.([a-zA-Z]+)$') as store_extension
+
+    FROM stg_dim_store__filtered
+),
+
+stg_dim_store__transform_extension AS (
+    SELECT 
+        store_url,
+        case lower(store_extension)
+            when 'com' then 'Global'
+            when 'au' then 'Australia'
+            when 'hk' then 'Hong Kong'
+            when 'cn' then 'China'
+            when 'ca' then 'Canada'
+            when 'pl' then 'Poland'
+            when 'cz' then 'Czech Republic'
+            when 'sk' then 'Slovakia'
+            when 'hu' then 'Hungary'
+            when 'ro' then 'Romania'
+            when 'br' then 'Brazil'
+            when 'ch' then 'Switzerland'
+            when 'de' then 'Germany'
+            when 'fr' then 'France'
+            when 'es' then 'Spain'
+            when 'pt' then 'Portugal'
+            when 'nl' then 'Netherlands'
+            when 'be' then 'Belgium'
+            when 'at' then 'Austria'
+            when 'se' then 'Sweden'
+            when 'no' then 'Norway'
+            when 'fi' then 'Finland'
+            when 'dk' then 'Denmark'
+            when 'si' then 'Slovenia'
+            when 'rs' then 'Serbia'
+            when 'ba' then 'Bosnia and Herzegovina'
+            when 'me' then 'Montenegro'
+            when 'hn' then 'Honduras'
+            when 'it' then 'Italy'
+            when 'gt' then 'Guatemala'
+            when 'sg' then 'Singapore'
+            when 'mx' then 'Mexico'
+            when 'local' then 'Local'
+            else 'Unknown'
+        end as store_extension
+    FROM stg_dim_store__extract_extension
+),
+
+stg_dim_store__store_name AS (
+    SELECT
+        store_url,
+        concat('Glamira', ' ', store_extension) as store_name,
+
+    FROM stg_dim_store__transform_extension
+),
+stg_dim_store__cast_type AS (
+    SELECT
+        CAST(store_url AS STRING) AS store_url,
+        CAST(store_name AS STRING) AS store_name
+    FROM stg_dim_store__store_name
+),
+stg_dim_store__dedup AS (
+    SELECT DISTINCT *
+    FROM stg_dim_store__cast_type
+),
+stg_dim_store__gen_key AS (
+    SELECT
+        store_url,
+        store_name,
+        FARM_FINGERPRINT(store_url || store_name) AS store_id
+    FROM stg_dim_store__dedup
+)
+SELECT *
+FROM stg_dim_store__gen_key
